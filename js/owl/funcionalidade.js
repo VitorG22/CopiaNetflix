f8546f45cbeaae75dfa102f19ba9d123038c5acc
VitorG22 <limavitor447@gmail.com>
2022-08-16 13:14:30 -0300

    commit inicial

---
+++ js/owl/funcionalidade.js
@@ -0,0 +1,103 @@
+let backg = document.getElementById('background');
+let titulo =document.getElementById('titulo');
+let desc = document.getElementById('descricao');
+let button = document.getElementById('assista');
+
+
+
+
+console.log(items.className);
+
+
+
+
+
+function Back0() {
+    backg.setAttribute('class', 'background0'); 
+    titulo.textContent = 'THOR: AMOR E TROVÃO'  
+    desc.textContent = 'Thor parte em uma jornada diferente de tudo que já enfrentou – uma busca pela paz interior. Mas sua aposentadoria é interrompida por um assassino galáctico conhecido como Gorr.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=1c_W_4cNLn0')
+}
+  
+function Back1(){
+    backg.setAttribute('class', 'background1'); 
+    titulo.textContent = 'A FERA DO MAR'  
+    desc.textContent = 'A vida de um caçador de monstros marinhos vira de cabeça para baixo depois que uma menina se esconde em seu navio.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=e-KPygLf5TA')
+}
+
+function Back2(){
+    backg.setAttribute('class', 'background2');   
+    titulo.textContent = 'A PRINCESA'  
+    desc.textContent = 'Quando uma princesa bela e determinada se recusa a se casar com o cruel sociopata para quem está prometida, ela é sequestrada e trancada em uma torre remota no castelo de seu pai. Com seu pretendente desprezado e vingativo querendo tomar o trono, a princesa deve proteger sua família e salvar o reino, custe o que custar.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=xaRV53H2mac')
+}
+
+function Back3(){
+    backg.setAttribute('class', 'background3');   
+    titulo.textContent = 'O PREDADOR: A CAÇADA'  
+    desc.textContent = 'Ambientado no mundo da Nação Comanche no início de 1700, "O Predador: A Caçada" é a história não contada de uma jovem guerreira altamente qualificada, desesperada para proteger seu povo do perigo iminente. Ela persegue e finalmente confronta sua presa, que acaba sendo um predador alienígena altamente evoluído com um arsenal tecnologicamente avançado, resultando em um confronto brutal e aterrorizante entre os adversários.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=CHFr3FCJN6c')
+}
+
+function Back4(){
+    backg.setAttribute('class', 'background4');   
+    titulo.textContent = 'A GUERRA DOS TRONOS'  
+    desc.textContent = 'Em uma terra onde os verões podem durar vários anos e o inverno toda uma vida, sete nobres famílias lutam pelo controle da mítica terra de Westeros, dividida depois de uma guerra. Num cenário que lembra a Europa medieval, reis, rainhas, cavaleiros e renegados usam todos os meios possíveis em um jogo político pela disputa do Trono de Ferro, o símbolo do poder absoluto.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=JH45HA1Ekqg&t=61s')
+}
+
+
+function Back5(){
+    backg.setAttribute('class', 'background5');   
+    titulo.textContent = 'HALO'  
+    desc.textContent = 'Uma épica batalha acontece no durante o século XXVI entre a raça humana e uma espécie alienígena conhecida como Covenant.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=7z7xJcOTja0')
+}
+
+function Back6(){
+    backg.setAttribute('class', 'background6');   
+    titulo.textContent = 'SANDMAN'  
+    desc.textContent = 'Um mago tenta capturar a Morte para barganhar pela vida eterna, no entanto, acaba prendendo seu irmão mais novo, Sonho. Temendo por sua segurança, o mago o mantém preso em uma garrafa de vidro por décadas. Após sua fuga, Sonho, também conhecido como Morpheus, parte em busca de seus poderosos objetos perdidos.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=fUg4xE-7LyM')
+}
+
+function Back7(){
+    backg.setAttribute('class', 'background7');   
+    titulo.textContent = 'STRANGER THINGS'  
+    desc.textContent = 'Quando um garoto desaparece, a cidade toda participa nas buscas. Mas o que encontram são segredos, forças sobrenaturais e uma menina.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=yTX0HxTq9wo&t=9s')
+}
+
+function Back8(){
+    backg.setAttribute('class', 'background8');   
+    titulo.textContent = 'THE UMBRELLA ACADEMY'  
+    desc.textContent = 'Com a morte do pai, irmãos com poderes extraordinários se reencontram e descobrem surpreendentes segredos de família – além de uma ameaça iminente à humanidade.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=uoaC6EXnMOM')
+}
+
+function Back9(){
+    backg.setAttribute('class', 'background9');   
+    titulo.textContent = 'A VIGILANTE DO AMANHA'  
+    desc.textContent = 'Num mundo pós 2029, cérebros se fundem facilmente a computadores e a tecnologia está em todos os lugares. Motoko Kusanagi é uma ciborgue com experiência militar que comanda um esquadrão de elite especializado em combater crimes cibernéticos.'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=cAcHMmv_Ld4')
+}
+
+function Back10(){
+    backg.setAttribute('class', 'background10');   
+    titulo.textContent = 'HOMEM-ARANHA: SEM VOLTA PARA CASA'   
+    desc.textContent = 'Peter Parker é desmascarado e não consegue mais separar sua vida normal dos grandes riscos de ser um super-herói. Quando ele pede ajuda ao Doutor Estranho, os riscos se tornam ainda mais perigosos, e o forçam a descobrir o que realmente significa ser o Homem-Aranha...'
+    button.setAttribute('href', 'https://www.youtube.com/watch?v=FDNNHh7TRN0')
+}
+
+
+      /* ////////////////////////////// */
+
+
+
+
+
+
+
+
+
